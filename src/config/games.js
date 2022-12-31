a2469fbc67953edb00f38b0baa91b48dc619bb79
batin <batin@batin.com>
2022-12-31 10:25:46 +0300

    updated games lib

--- src/config/games.js
+++ src/config/games.js
@@ -3,17 +3,17 @@
 // Example:
 // {
 //     name: "Just Dance 2014", Full name of the game
-//     available: true, If the game is allowed to access services
+//     isAvailable: true, If the game is allowed to access services
 //     Region games
 //     regions: {
 //            Region ID
 //            SJOP: {
 //                region: PAL,
-//                available: true You can also enable/disable certain regions
+//                isAvailable: true You can also enable/disable certain regions
 //            },
 //            SJOE: {
 //                region: NTSC,
-//                available: true
+//                isAvailable: true
 //            }
 //     },
 //     We keep the game's star count, amount of songs and unlocksCount here to detect any cheating
@@ -30,15 +30,15 @@ const NTSC = "NTSC";
 module.exports = [{
     name: "Just Dance 2014",
     version: 2014,
-    available: true,
+    isAvailable: false,
     regions: {
         SJOP: {
             region: PAL,
-            available: true
+            isAvailable: true
         },
         SJOE: {
             region: NTSC,
-            available: true
+            isAvailable: true
         }
     },
     stats: {
@@ -49,45 +49,45 @@ module.exports = [{
 }, {
     name: "Just Dance 2015",
     version: 2015,
-    available: true,
+    isAvailable: true,
     regions: {
         SE3P: {
             region: PAL,
-            available: true
+            isAvailable: true
         },
         SE3E: {
             region: NTSC,
-            available: true
+            isAvailable: true
         }
     },
     stats: {}
 }, {
     name: "Just Dance 2016",
     version: 2016,
-    available: true,
+    isAvailable: true,
     regions: {
         SJNP: {
             region: PAL,
-            available: true
+            isAvailable: true
         },
         SJNE: {
             region: NTSC,
-            available: true
+            isAvailable: true
         }
     },
     stats: {}
 }, {
     name: "Just Dance 2017",
     version: 2017,
-    available: true,
+    isAvailable: true,
     regions: {
         SZ7P: {
             region: PAL,
-            available: true
+            isAvailable: true
         },
         SZ7E: {
             region: NTSC,
-            available: true
+            isAvailable: true
         }
     },
     stats: {
@@ -98,15 +98,15 @@ module.exports = [{
 }, {
     name: "Just Dance 2018",
     version: 2018,
-    available: true,
+    isAvailable: true,
     regions: {
         SE8P: {
             region: PAL,
-            available: true
+            isAvailable: true
         },
         SE8E: {
             region: NTSC,
-            available: true
+            isAvailable: true
         }
     },
     stats: {
--- src/lib/games.js
+++ src/lib/games.js
@@ -1,23 +1,61 @@
 class Games {
     constructor() {
-        let games = global.gs.GAMES;
-        this.games = [];
-
-        // Turn games object to an array and append key as "version"
-        Object.keys(games).forEach(k => {
-            this.games.push({
-                ...games[k],
-                version: k
-            })
-        });
+        this.games = global.gs.GAMES;
     }
 
+    /**
+     * Find game by your own filter function
+     * @param {*} fn 
+     * @returns 
+     */
+    getGame(fn) {
+        return this.games.filter(fn)[0] || null;
+    }
+
+    /**
+     * Get game by gameId
+     * @param {*} id 
+     * @returns 
+     */
+    getGameById(id) {
+        return this.games.filter(g => g.regions.hasOwnProperty(id))[0] || null;
+    }
+
+    /**
+     * Get game by version
+     * @param {*} version 
+     * @returns 
+     */
     getGameByVersion(version) {
         return this.games.filter(g => g.version == version)[0] || null;
     }
 
-    getGameById(id) {
-        return this.games.filter(g => g.version == version)[0] || null;
+    /**
+     * Checks if game is available
+     * If region is provided, it will return if region is available
+     * but if gameId is provided it will check if the game is available
+     * @param {*} idOrVersion 
+     * @returns 
+     */
+    isGameAvailable(idOrVersion) {
+        let game = this.getGameById(idOrVersion) || this.getGameByVersion(idOrVersion);
+        if (!game) return false;
+
+        let isRegion = game.regions.hasOwnProperty(idOrVersion);
+        if (isRegion && game.isAvailable) return game.regions[idOrVersion]?.isAvailable || false;
+
+        return game.isAvailable;
+    }
+
+    /**
+     * Returns game's statistics
+     * @param {*} idOrVersion 
+     * @returns 
+     */
+    getGameStats(idOrVersion) {
+        let game = this.getGameById(idOrVersion) || this.getGameByVersion(idOrVersion);
+        if (!game) return null;
+        return game.stats;
     }
 }
 
